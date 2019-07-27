f236b838a933beb9bd807b17d97c09dfa97157c0
elliottprogrammer <bryan@elliottprogrammer.com>
2019-07-27 12:18:41 -0700

    add db connection & User model

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const express = require('express');
 const app = express();
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+
+dotenv.config();
+
+// connect to DB
+mongoose.connect(process.env.MLAB_URI, { useNewUrlParser: true }, () =>
+    console.log('connected to DB!')
+);
 
 // import routes
 const authRoute = require('./routes/auth');
