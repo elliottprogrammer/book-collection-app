39f341e776c78aa97ff989353195bdbe5a70c28a
elliottprogrammer <bryan@elliottprogrammer.com>
2019-08-11 12:50:33 -0700

    prep for heroku deploy

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use('/api/books', bookRoute);
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
 
-    app.length('*', (req, res) => {
+    app.get('*', (req, res) => {
         res.sendfile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
